439f737edbc52484582ee72a4258ea73bc17980c
Oldarorn <36448848+Oldarorn@users.noreply.github.com>
2019-03-20 11:26:12 +0100

    Create fr.lua

---
+++ locales/fr.lua
@@ -0,0 +1,9 @@
+Locales['fr'] = {
+  ['cancel'] = 'annuler',
+  ['vehicle_deny'] = '~r~Action impossible depuis un véhicule!',
+  ['wait_end'] = '~r~Vous devez attendre la fin de l\'action',
+  ['wrong_place'] = '~r~Action impossible à cet endroit.',
+  ['doing_action'] = '~g~Plantation en cours...',
+  ['how_cancel'] = 'pour annuler l\'action cliquez sur ce bouton : <button type='button'>X</button>',
+  ['wait_ten_sec'] = '~r~Un peu de patience...',
+}

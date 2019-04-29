4ffdc700ce7cd28cfa3e28b39e9211fdc4d0c096
Oldarorn <36448848+Oldarorn@users.noreply.github.com>
2019-04-29 13:38:15 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -5,15 +5,15 @@ Discription "Receptury by mWojtasik (Oesis.pl)"
 Version "2.0"
 
 server_scripts {
-    '@es_extended/locale.lua',
-    'config.lua',
-    'server/main.lua'
+  '@es_extended/locale.lua',
+  'config.lua',
+  'server/main.lua'
 }
 
 client_scripts {
-    'config.lua',
-    '@es_extended/locale.lua',
-    'client/main.lua',
-    'locales/pl.lua',
-    'locales/fr.lua'    
+  'config.lua',
+  '@es_extended/locale.lua',
+  'client/main.lua',
+  'locales/pl.lua',
+  'locales/fr.lua'    
 }

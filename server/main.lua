01adb99cefdd12da6511434864f4ad8c55b3a242
Oldarorn <36448848+Oldarorn@users.noreply.github.com>
2019-04-29 02:52:26 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,54 +1,53 @@
 ESX 			    			= nil
 local lastTime = nil
-local Config = {}
-Config.CopsOnDuty = 1
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 
-ESX.RegisterUsableItem('seed_weed', function(source)
-	local _source = source
-	local currentTime = os.time(os.date("!*t"))
-	if lastTime and currentTime - lastTime < 10 then
-		TriggerClientEvent("pNotify:SendNotification", source, {
-		    text = _U('waitconfig'),
-		    type = "error",
-		    timeout = 2000,
-		    layout = "centerLeft"
-		})
-		do return end
-	end
-	lastTime = os.time(os.date("!*t"))
+for item_name in pairs(options) do
+    ESX.RegisterUsableItem(item_name, function(source)
+        local _source = source
+        local currentTime = os.time(os.date("!*t"))
+        if lastTime and currentTime - lastTime < 10 then
+            TriggerClientEvent("pNotify:SendNotification", source, {
+                text = _U('waitconfig'),
+                type = "error",
+                timeout = 2000,
+                layout = "centerLeft"
+            })
+            do return end
+        end
+        lastTime = os.time(os.date("!*t"))
 
-	--Sprawdź czy jest wystarczająca liczba glin
-	local xPlayers = ESX.GetPlayers()
-	local cops = 0
-	for i=1, #xPlayers, 1 do
-	 local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-	 if xPlayer.job.name == 'police' then
-					cops = cops + 1
-					if cops >= Config.CopsOnDuty then break end
-			end
-	end
+        --Check is there a good count of cops on duty
+        local xPlayers = ESX.GetPlayers()
+        local cops = 0
+        for i=1, #xPlayers, 1 do
+            local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+            if xPlayer.job.name == 'police' then
+                cops = cops + 1
+                if cops >= options[item_name].cops then break end
+            end
+        end
 
-	if cops >= Config.CopsOnDuty then
-		TriggerClientEvent('esx_receptury:RequestStart', _source, 'seed_weed', lastTime)
-	else
-		TriggerClientEvent("pNotify:SendNotification", _source, {
-		    text = _U('police_req_1') .. Config.CopsOnDuty .. _U('police_req_2'),
-		    type = "error",
-		    timeout = 5000,
-		    layout = "centerLeft"
-		})
-		TriggerClientEvent("pNotify:SendNotification", _source, {
-					text =  .. Config.CopsOnDuty .. _U('police2'),
-		    text = _U('police') .. cops ..'/'.. Config.CopsOnDuty,
-		    type = "info",
-		    timeout = 6000,
-		    layout = "centerLeft"
-		})
-	end
-end)
+        if cops >= options[item_name].cops then
+            TriggerClientEvent('esx_receptury:RequestStart', _source, item_name, lastTime)
+        else
+            TriggerClientEvent("pNotify:SendNotification", _source, {
+                 text = _U('police_req_1') .. options[item_name].cops .. _U('police_req_2'),
+                type = "error",
+                timeout = 5000,
+                layout = "centerLeft"
+            })
+            TriggerClientEvent("pNotify:SendNotification", _source, {
+                text = text = _U('police') .. cops ..'/'.. options[item_name].cops,
+                type = "info",
+                timeout = 6000,
+                layout = "centerLeft"
+            })
+        end
+    end)
+end
 
 
 RegisterServerEvent("esx_receptury:RemoveItem")
